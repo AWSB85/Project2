b2fa789b7a5c642af5686f3f95114625a46b7055
awsbasiya <awsbasiya@localhost>
2023-04-05 23:12:01 -0400

    aaassssss

---
+++ src/demo/Test.java
@@ -0,0 +1,14 @@
+package demo;
+
+public class Test {
+	
+	public static void main (String [] args) {
+		
+System.out.println("Test........");
+		
+		System.out.println("Git Lecture......");
+		
+		
+	}
+
+}
